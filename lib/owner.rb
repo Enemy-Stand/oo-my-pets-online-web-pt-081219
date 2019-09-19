e3952351d5e7f439c7df765a6cd3ead5eb86c901
Joseph Sesti <sestijoey63@gmail.com>
2019-09-19 17:46:56 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -48,4 +48,9 @@ class Owner
   def feed_cats
     self.cats.each {|cat| cat.mood = "happy"}
   end
+  
+  def sell_pets
+    self.cats.each {|cat| cat.mood = "nervous"}
+    self.dogs.each {|dog| dog.mood = "nervous"}
+  end
 end
